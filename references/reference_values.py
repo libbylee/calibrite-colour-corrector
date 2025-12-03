e6b7265184bb1b644084cf14c712844f523883cf
libbylee <leexlibby@gmail.com>
2025-12-02 17:17:20 -0800

    Added reference l*a*b values from X-rite data file

---
+++ references/reference_values.py
@@ -0,0 +1,33 @@
+# ColorChecker Classic 24-patch reference L*a*b* values
+# From X-Rite official data file (November 2014 edition and newer)
+# Measured with i1Pro 2, MeasurementCondition=M0, Filter=no
+# Order: A1, B1, C1, D1, E1, F1, A2, B2, C2, D2, E2, F2, A3, B3, C3, D3, E3, F3, A4, B4, C4, D4, E4, F4
+
+
+COLORCHECKER_LAB_REFERENCES = [
+    (37.54, 14.37, 14.92),    # A1 - Dark skin
+    (64.66, 19.27, 17.50),    # B1 - Light skin
+    (49.32, -3.82, -22.54),   # C1 - Blue sky
+    (43.46, -12.74, 22.72),   # D1 - Foliage
+    (54.94, 9.61, -24.79),    # E1 - Blue flower
+    (70.48, -32.26, -0.37),   # F1 - Bluish green
+    (62.73, 35.83, 56.50),    # A2 - Orange
+    (39.43, 10.75, -45.17),   # B2 - Purplish blue
+    (50.57, 48.64, 16.67),    # C2 - Moderate red
+    (30.10, 22.54, -20.87),   # D2 - Purple
+    (71.77, -24.13, 58.19),   # E2 - Yellow green
+    (71.51, 18.24, 67.37),    # F2 - Orange yellow
+    (28.37, 15.42, -49.80),   # A3 - Blue
+    (54.38, -39.72, 32.27),   # B3 - Green
+    (42.43, 51.05, 28.62),    # C3 - Red
+    (81.80, 2.67, 80.41),     # D3 - Yellow
+    (50.63, 51.28, -14.12),   # E3 - Magenta
+    (49.57, -29.71, -28.32),  # F3 - Cyan
+    (95.19, -1.03, 2.93),     # A4 - White
+    (81.29, -0.57, 0.44),     # B4 - Neutral 8
+    (66.89, -0.75, -0.06),    # C4 - Neutral 6.5
+    (50.76, -0.13, 0.14),     # D4 - Neutral 5
+    (35.63, -0.46, -0.48),    # E4 - Neutral 3.5
+    (20.64, 0.07, -0.46),     # F4 - Black
+]
+
